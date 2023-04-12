0e63bc7204cffe42bac4b43f151be644c770db3a
gonzaap <gonzatrax@gmail.com>
2023-04-12 13:08:12 +0200

    empezamos de 0

--- P2/tienda.js
+++ P2/tienda.js
@@ -2,9 +2,6 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const URL = require('url').URL;
-const filePath = path.join(__dirname, 'usuarios.json');
-
-
 
 const server = http.createServer((req, res) => {
   // obtén la ruta del archivo solicitado
@@ -16,52 +13,30 @@ const server = http.createServer((req, res) => {
     if (filePath === path.join(__dirname, '/')) {
     filePath = path.join(__dirname, '/index.html');
     }
-if (req.method === 'POST' && req.url === '/usuarios') {
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk.toString();
-    });
-    req.on('end', () => {
-      try {
-        const usuario = JSON.parse(body);
-        registrarUsuario(usuario);
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Usuario registrado con éxito' }));
-      } catch (error) {
-        console.error(error);
-        res.writeHead(400, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Error al registrar usuario' }));
-      }
-    });
-  }else {
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Archivo no encontrado\n');
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Error 404:Archivo no encontrado\n');
+    } else {
+      if (filePath.endsWith('.jpg') || filePath.endsWith('.jpeg')) {
+        contentType = 'image/jpeg';
+      } else if (filePath.endsWith('.png')) {
+        contentType = 'image/png';
+      } else if (filePath.endsWith('.gif')) {
+        contentType = 'image/gif';
       } else {
-        let contentType;
-        if (filePath.endsWith('.jpg') || filePath.endsWith('.jpeg')) {
-          contentType = 'image/jpeg';
-        } else if (filePath.endsWith('.png')) {
-          contentType = 'image/png';
-        } else if (filePath.endsWith('.gif')) {
-          contentType = 'image/gif';
-        } else {
-          contentType = 'text/html';
-        }
-        res.statusCode = 200;
-        res.setHeader('Content-Type', contentType);
-        res.writeHead(200, 'Content-Type' );
+        contentType = 'text/html';
+      }
+      res.statusCode = 200;
+      res.writeHead(200, 'Content-Type' );
         console.log("Recurso recibido: " );
         console.log("200 OK");
       res.end(data);
-        res.end(data);
-      }
-    });
-  }
-   
+    }
   });
+});
 
 server.listen(9000, () => {
   console.log('Servidor corriendo en http://localhost:9000/');
