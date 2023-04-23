1a1d41dee0c2b0ee51b53591f16399f1f67ee13c
gonzaap <gonzatrax@gmail.com>
2023-04-23 18:31:19 +0200

    registro implementado, registra cualquiera, mal

--- P2/tienda.js
+++ P2/tienda.js
@@ -3,119 +3,118 @@ const fs = require('fs');
 const path = require('path');
 const URL = require('url').URL;
 
-
 // Objeto para almacenar detalles de sesión del usuario
 const session = {};
 
-// Agregar usuarios de prueba
-registrarUsuario({ email: 'usuarioprueba', password: 'usuarioprueba' });
-registrarUsuario({ email: 'root', password: 'root' });
-
+// Leer archivo de usuarios
+const filePath = path.join(__dirname, 'usuarios.json');
+let usuariosJSON = fs.readFileSync(filePath);
+let usuarios = JSON.parse(usuariosJSON).usuarios;
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
-    let filePath = '.' + req.url;
-    if (filePath === './') {
-      filePath = './index.html';
-    }
-    const extname = String(path.extname(filePath)).toLowerCase();
-    let contentType = 'text/html';
-    switch (extname) {
-      case '.js':
-        contentType = 'text/javascript';
-        break;
-      case '.css':
-        contentType = 'text/css';
-        break;
-      case '.json':
-        contentType = 'application/json';
-        break;
-      case '.png':
-        contentType = 'image/png';
-        break;
-      case '.jpg':
-        contentType = 'image/jpg';
-        break;
-      case '.gif':
-        contentType = 'image/gif';
-        break;
-    }
-    fs.readFile(filePath, (error, content) => {
-      if (error) {
-        enviarRespuestaError(res, 500, 'Error en el servidor');
-        console.error(error);
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const usuario = url.searchParams.get('usuario');
+    const contrasena = url.searchParams.get('contrasena');
+    if (usuario && contrasena) {
+      const datosUsuario = JSON.parse(body);
+      if (datosUsuario.usuario && datosUsuario.contrasena) {
+        if (validarUsuario(datosUsuario.usuario, datosUsuario.contrasena)) {
+          req.session = {
+            usuario: datosUsuario.usuario,
+            loggedIn: true
+          };
+          redirigirPagina('/index.html', res);
+        } else {
+          enviarRespuestaError(res, 401, 'Usuario o contraseña incorrectos');
+          console.error(error);
+        }
       } else {
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(content, 'utf-8');
+        enviarRespuestaError(res, 400, 'Faltan campos en el formulario');
+        console.error(error);
       }
-    });
+    } else {
+      let filePath = '.' + url.pathname;
+      if (filePath === './') {
+        filePath = './index.html';
+      }
+      const extname = String(path.extname(filePath)).toLowerCase();
+      let contentType = 'text/html';
+      switch (extname) {
+        case '.js':
+          contentType = 'text/javascript';
+          break;
+        case '.css':
+          contentType = 'text/css';
+          break;
+        case '.json':
+          contentType = 'application/json';
+          break;
+        case '.png':
+          contentType = 'image/png';
+          break;
+        case '.jpg':
+          contentType = 'image/jpg';
+          break;
+        case '.gif':
+          contentType = 'image/gif';
+          break;
+      }
+      fs.readFile(filePath, (error, content) => {
+        if (error) {
+          enviarRespuestaError(res, 500, 'Error en el servidor');
+          console.error(error);
+        } else {
+          res.writeHead(200, { 'Content-Type': contentType });
+          res.end(content, 'utf-8');
+        }
+      });
+    }
   } else if (req.method === 'POST') {
     let body = '';
+    let filePath = '.' + req.url.split('?')[0];
     req.on('data', (chunk) => {
       body += chunk.toString();
     });
     req.on('end', () => {
-      const usuario = JSON.parse(body);
-      if (usuario.username && usuario.password) {
-        if (usuarios[usuario.username] === usuario.password) {
+      const datosUsuario = JSON.parse(body);
+      if (datosUsuario.usuario && datosUsuario.contrasena) {
+        if (validarUsuario(datosUsuario.usuario, datosUsuario.contrasena)) {
           req.session = {
-            username: usuario.username,
+            usuario: datosUsuario.usuario,
             loggedIn: true
           };
-          redirigirPagina('/', res);
+          redirigirPagina('/index.html', res);
         } else {
           enviarRespuestaError(res, 401, 'Usuario o contraseña incorrectos');
+          console.error(error);
         }
       } else {
         enviarRespuestaError(res, 400, 'Faltan campos en el formulario');
+        console.error(error);
       }
     });
   } else {
     enviarRespuestaError(res, 405, 'Método no permitido');
+    console.error(error);
   }
 });
-
-function registrarUsuario(usuario) {
-  const filePath = path.join(__dirname, 'usuarios.json');
-  try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const usuariosJSON = JSON.parse(data);
-  
-    // agrega el nuevo usuario al array de usuarios
-    usuariosJSON.usuarios.push(usuario);
-  
-    // escribe el archivo JSON actualizado de vuelta al disco
-    fs.writeFileSync(filePath, JSON.stringify(usuariosJSON));
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error al registrar usuario');
-  }
-}
 
 function enviarRespuestaError(res, statusCode, mensaje) {
   res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
   res.end(mensaje);
 }
 
-function validarUsuario(usuario) {
-  const filePath = path.join(__dirname, 'usuarios.json');
-  try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const usuariosJSON = JSON.parse(data);
-    const usuarios = usuariosJSON.usuarios;
-    const usuarioValido = usuarios.find(u => u.email === usuario.email && u.password === usuario.password);
-    return usuarioValido !== undefined;
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error al validar usuario');
-  }
+function validarUsuario(usuario, contrasena) {
+  const usuarioValido = usuarios.find(u => u.usuario === usuario && u.contrasena === contrasena);
+  return usuarioValido !== undefined;
 }
 
 function redirigirPagina(url, res) {
   res.writeHead(303, { 'Location': url });
   res.end();
 }
-  
-server.listen(9000, () => {
+
+server.listen(8090, () => {
   console.log('Servidor corriendo en http://localhost:9000/');
 });
